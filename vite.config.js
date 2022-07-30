7649904e7a9b20cb1aecf04fc8afabbdc7d5306e
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2022-07-30 14:38:52 +0900

    about作成

--- vite.config.js
+++ vite.config.js
@@ -12,14 +12,14 @@ export default defineConfig({
             input: glob.sync(resolve(__dirname, "src", "**/*.html")),// 下層ページも出力する
             output: {
                 assetFileNames: (assetInfo) => {
+                    console.log(assetInfo);
                     const extType = assetInfo.name.split('.')[1];
-                    console.log(extType);
                     if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
                         return `assets/images/[name][extname]`;
                     }
-                    if (extType === 'css') {
-                        return `assets/css/style.css`;
-                    }
+                    // if (extType === 'css') {
+                    //     return `assets/css/style.css`;
+                    // }
                     return `assets/${extType}/[name][extname]`;
                 },
                 chunkFileNames: 'assets/js/[name].js',
