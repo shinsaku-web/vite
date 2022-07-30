269b1ed55effce2a063402a62e907c2233b62872
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2022-07-30 13:35:57 +0900

    assetのバンドルファイル名を設定

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,24 @@ export default defineConfig({
     publicDir: '../public',
     build: {
         outDir: '../dist',
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    let extType = assetInfo.name.split('.')[1];
+                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+                        extType = 'images';
+                    }
+                    if (extType === 'css') {
+                        return `assets/css/style.css`;
+                    }
+                    return `assets/${extType}/[name][extname]`;
+                },
+                chunkFileNames: 'assets/js/[name].js',
+                entryFileNames: 'assets/js/[name].js',
+            },
+        }
     },
     server: {
         open: '/',
-    }
+    },
 });
